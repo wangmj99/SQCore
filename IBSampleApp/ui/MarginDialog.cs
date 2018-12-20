336bfa31f4a90b3c87f50baf99e9e81a6e10fa62
wangmj99 <45906771+wangmj99@users.noreply.github.com>
2018-12-20 16:42:47 +0800

    Add files via upload

---
+++ IBSampleApp/ui/MarginDialog.cs
@@ -0,0 +1,60 @@
+﻿/* Copyright (C) 2018 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
+ * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using IBSampleApp.messages;
+using IBApi;
+
+namespace IBSampleApp.ui
+{
+    partial class MarginDialog : Form
+    {
+        delegate void UpdateMarginInformationDelegate(OrderState state);
+
+        public MarginDialog()
+        {
+            InitializeComponent();
+        }
+
+        public void FillAndDisplay(OrderState state)
+        {
+            this.equityWithLoanBefore.Text = Util.formatDoubleString(state.EquityWithLoanBefore);
+            this.initialMarginBefore.Text = Util.formatDoubleString(state.InitMarginBefore);
+            this.maintenanceMarginBefore.Text = Util.formatDoubleString(state.MaintMarginBefore);
+            this.equityWithLoanChange.Text = Util.formatDoubleString(state.EquityWithLoanChange);
+            this.initialMarginChange.Text = Util.formatDoubleString(state.InitMarginChange);
+            this.maintenanceMarginChange.Text = Util.formatDoubleString(state.MaintMarginChange);
+            this.equityWithLoanAfter.Text = Util.formatDoubleString(state.EquityWithLoanAfter);
+            this.initialMarginAfter.Text = Util.formatDoubleString(state.InitMarginAfter);
+            this.maintenanceMarginAfter.Text = Util.formatDoubleString(state.MaintMarginAfter);
+        }
+
+        public void UpdateMarginInformation(OrderState state)
+        {
+            FillAndDisplay(state);
+            this.ShowDialog();  
+        }
+
+        private void acceptMarginButton_Click(object sender, EventArgs e)
+        {
+            this.equityWithLoanBefore.Text = "";
+            this.initialMarginBefore.Text = "";
+            this.maintenanceMarginBefore.Text = "";
+            this.equityWithLoanChange.Text = "";
+            this.initialMarginChange.Text = "";
+            this.maintenanceMarginChange.Text = "";
+            this.equityWithLoanAfter.Text = "";
+            this.initialMarginAfter.Text = "";
+            this.maintenanceMarginAfter.Text = "";
+            this.Visible = false;
+        }
+
+    }
+}
---
+++ IBSampleApp/ui/PnLManager.cs
@@ -0,0 +1,56 @@
+﻿/* Copyright (C) 2018 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
+ * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IBSampleApp.ui
+{
+    class PnLManager
+    {
+        private int pnlReqId = 0;
+        private IBClient ibClient;
+        private int pnlSingleReqId = 0;
+
+        public void ReqPnL(string account, string modelCode)
+        {
+            pnlReqId = new Random(DateTime.Now.Millisecond).Next();
+
+            ibClient.ClientSocket.reqPnL(pnlReqId, account, modelCode);
+        }
+
+        public void CancelPnL()
+        {
+            if (pnlReqId != 0)
+            {
+                ibClient.ClientSocket.cancelPnL(pnlReqId);
+
+                pnlReqId = 0;
+            }
+        }
+
+        public void ReqPnLSingle(string account, string modelCode, int conId)
+        {
+            pnlSingleReqId = new Random(DateTime.Now.Millisecond).Next();
+
+            ibClient.ClientSocket.reqPnLSingle(pnlSingleReqId, account, modelCode, conId);
+        }
+
+        public void CancelPnLSingle()
+        {
+            if (pnlSingleReqId != 0)
+            {
+                ibClient.ClientSocket.cancelPnLSingle(pnlSingleReqId);
+
+                pnlSingleReqId = 0;
+            }
+        }
+
+        public PnLManager(IBClient ibClient)
+        {
+            this.ibClient = ibClient;
+        }
+    }
+}
